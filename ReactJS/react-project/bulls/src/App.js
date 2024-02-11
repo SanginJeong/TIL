68855fe253ba61bf10d008cde2cefd4eb3d59dc7
SangIn Jeong <sanginjeong07@gmail.com>
2024-02-12 05:26:47 +0900

    2024-02-12

--- ReactJS/react-project/bulls/src/App.js
+++ ReactJS/react-project/bulls/src/App.js
@@ -2,55 +2,98 @@ import logo from './logo.svg';
 import './App.css';
 import {useState, useRef, useEffect, useCallback} from 'react';
 import React from 'react';
-// - 유저는 숫자를 입력할 수 있다.
-// - 유저는 남은 기회를 볼 수 있다.
-// - 유저는 로그를 볼 수 있다.
-//  아래는 편의기능
-// - 이미 제출한 값이라면 유저에게 알려주고 기회를 카운트하지 않는다.
-// - 4글자까지만 입력제한을 둔다.
-// - 페이지가 로딩될 시 input태그에 focus한다.
-// - 제출 후에도 input태그에 focus한다.
-function App() {
-  useEffect(()=>{inputEl.current.focus()},[])
 
-  const inputEl = React.useRef(null);
-  const [chance, setChance] = useState(10);
-  const [log, setLog] = useState([{
-    number : '0123',
-    result : 'out',
-  }]);
-  const [value, setValue] = useState('');
+
+const generateAnswerList = () => {
   const list = Array(10).fill(0).map((v,i)=>i);
-  const answerList = [];
+  const result = [];
   for(let i=0; i<4; i++){
     const index = ~~(Math.random() * (list.length-1));
-    answerList.push(list[index]);
+    result.push(list[index]);
     list.splice(index,1);
   }
-  console.log('정답 : ',answerList);
+  return result;
+}
 
-  const submitNumber = useCallback((event) => {
+function App() {
+  useEffect(()=>{inputEl.current.focus()},[]);
+  const inputEl = React.useRef(null);
+  const [answer, setAnswer] = useState(generateAnswerList());
+  const [chance, setChance] = useState(10);
+  const [log, setLog] = useState([]);
+  const [value, setValue] = useState('');
+  const [isEnd, setIsEnd] = useState(false);
+
+  useEffect(()=>{console.log(answer);},[answer]);
+
+  const submitNumber = (event) => {
     event.preventDefault();
-    setLog([...log,{number: value}]);
-  },[value])
+    if(chance < 1){
+      return;
+    }
+    if(value.length < 4){
+      alert('4자리수를 제출하세요')
+      return
+    }
+    if(answer.join('') === value){
+      setLog([...log,{number: value, result: '정답입니다.'}]);
+      setIsEnd(true);
+      return
+    }
 
-  const onChangeInput = useCallback((e) => setValue(e.target.value), []);
+    let strike = 0;
+    let ball = 0;
+    const arr = value.split('').map(Number);
+    for(let i=0; i<arr.length; i++){
+      for(let j=0; j<answer.length; j++){
+        if(i === j && arr[i] === answer[j]){
+          strike++;
+        }
+        else if(arr[i] === answer[j]){
+          ball++;
+        }
+      }
+    }
+    setLog([...log,{number: value, result: 
+      (strike === 0 && ball === 0) ? `OUT` : `${strike}S ${ball}B`}]);
+    setChance(chance-1);
+    setValue('');
+  }
+
+  const reset = () => {
+    setAnswer(generateAnswerList());
+    setIsEnd(false);
+    setLog([]);
+    setChance(10);
+    setValue('');
+    inputEl.current.focus()
+  }
   return (
     <>
     <div className='board'>
       <form onSubmit={submitNumber}>
-        <input ref={inputEl} type="number" value={value} onChange={onChangeInput}/>
-        <button>입력</button>
+        <input 
+        onInput={(e) => {
+          if (e.target.value.length > e.target.maxLength)
+            e.target.value = e.target.value.slice(0, e.target.maxLength);
+        }}
+        ref={inputEl} 
+        type="number" 
+        value={value} 
+        maxLength={4} 
+        onChange={(e)=>{setValue(e.target.value)}}/>
+        <button onClick={submitNumber}>입력</button>
       </form>
-      <div>{chance}</div>
-      {log.map((submit)=>{
+      <div>남은기회 : {chance}</div>
+      {log.map((submit,i)=>{
         return (
           <div className='log'>
-            <div>{submit.number}</div>
+            <div>{`${i+1}차 시도`} {submit.number}</div>
             <div>{submit.result}</div>
           </div>
         )
       })}
+      {isEnd || chance ===0 ? <button onClick={reset}>다시하기</button>: ''}
     </div>
     </>
   );
