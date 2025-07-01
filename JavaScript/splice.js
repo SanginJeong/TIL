525380c748cb53c8e9300aa9a462fc48254130ee
SangIn Jeong <sanginjeong07@gmail.com>
2025-07-01 09:49:47 +0900

    splice : 3rd parameter

---
+++ JavaScript/splice.js
@@ -0,0 +1,13 @@
+// splice 의 세번째 요소는 해당 자리에 추가할 값이다.
+
+const arr = [0,1,2,3];
+
+// 한 개 삭제
+arr.splice(1,1,5);
+console.log(arr);
+
+// 여러 개 삭제 -> 여러개를 삭제하고 5를 넣음
+arr.splice(1,2,5);
+
+
+console.log(arr);
