862f63b85e942f1d82b044ef6f6d1591f272da2d
SangIn Jeong <sanginjeong07@gmail.com>
2025-07-01 10:46:36 +0900

    immutable string

---
+++ JavaScript/toString.js
@@ -0,0 +1,8 @@
+let num = 255;
+
+console.log(num.toString(2));
+console.log(num.toString(8));
+console.log(num.toString(16));
+console.log(num.toString(32));
+console.log(255..toString(2)); // 숫자 뒤의 . 한개는 소수점으로 인식할 수 있어서 두개붙여야한다.
+console.log((255).toString(2)); // 혹은 괄호
