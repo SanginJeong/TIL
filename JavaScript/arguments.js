119e4bcd2a0d1e60a9eac77df8881941388b32f2
sanginjeong <sanginjeong07@gmail.com>
2025-07-07 13:27:06 +0900

    rest parameter

--- JavaScript/arguments.js
+++ JavaScript/arguments.js
@@ -8,3 +8,11 @@ function word() {
 }
 
 word('나만', '없어', '고양이');
+
+// rest parameter : arguments 객체와 다르게 배열이라는 점이 여러 메서드를 사용하기에 좋다.
+
+function test(...args) {
+  console.log(args.filter((v)=>v%2===0));
+}
+
+test(1,2,3,4,5) // 2,4
